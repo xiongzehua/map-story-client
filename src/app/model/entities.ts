3fbdb4acbe3eaa419e8de2a30ab00771b994e549
xiong <313398439@qq.com>
2019-01-18 00:05:20 +0800

    删除无关代码

--- src/app/model/entities.ts
+++ src/app/model/entities.ts
@@ -9,20 +9,6 @@ export class Card {
   public content: string;
 }
 
-export class Release {
-  public index: number;
-  public cards: Card[];
-  public info(): string {
-    if (this.index === -1) {
-      return 'User activities';
-    } else if (this.index === 0) {
-      return 'User tasks';
-    } else {
-      return `User stories Release${this.index}`;
-    }
-  }
-}
-
 export class User {
   constructor(
     public id: number,
